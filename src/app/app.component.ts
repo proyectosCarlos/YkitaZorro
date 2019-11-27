3ea21ffbd8d40f3be2e8d2d3c4f344dee89b1c00
carlos alberto taborda ramirez <ca.taborda.ramirez@gmail.com>
2019-11-27 12:15:37 -0500

    drawer + comentarios base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NzIconService } from 'ng-zorro-antd/icon';
+import { addDays, distanceInWords } from 'date-fns';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +10,31 @@ import { NzIconService } from 'ng-zorro-antd/icon';
 export class AppComponent {
   title = 'ctpzorro';
 
+  isVisible = false;
+  visible = false;
+
   constructor(private iconService: NzIconService) {
     this.iconService.fetchFromIconfont({
       scriptUrl: 'https://at.alicdn.com/t/font_8d5l8fzk5b87iudi.js'
     });
   }  
 
-  visible = false;
+  showModal(): void {
+    this.isVisible = true;
+  }
 
+  handleOk(): void {
+    console.log('Button ok clicked!');
+    this.isVisible = false;
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible = false;
+  }
+
+
+  placement = 'right';
   open(): void {
     this.visible = true;
   }
@@ -24,4 +42,36 @@ export class AppComponent {
   close(): void {
     this.visible = false;
   }
+
+
+  data = [
+    {
+      title: 'Ant Design Title 1'
+    },
+    {
+      title: 'Ant Design Title 2'
+    },
+    {
+      title: 'Ant Design Title 3'
+    },
+    {
+      title: 'Ant Design Title 4'
+    },
+    {
+      title: 'Ant Design Title 4'
+    },
+    {
+      title: 'Ant Design Title 4'
+    },
+    {
+      title: 'Ant Design Title 4'
+    }
+  ];
+
+  user = {
+    author: 'Han Solo',
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png'
+  };
+
+
 }
